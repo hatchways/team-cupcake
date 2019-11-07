0d6ada66e8d136b19dc9f60c429e4282f42a8ed4
shojol <dip_shojol@yahoo.com>
2019-11-07 15:03:17 -0500

    follow and message button corrected

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import Login from "./Components/LoginForm";
 import SignUp from "./Components/SignupForm";
 // import LandingPage from "./pages/Landing";
 import Profile from "./pages/Profile";
+// import ShareMusic from "./pages/ShareMusic";
 import Discover from "./pages/Discover";
 import Messages from "./pages/Messages";
 import Nav from "./Components/Nav";
@@ -15,13 +16,13 @@ import "./App.css";
 
 function App() {
   // when true, user logged in and will not be redirected to dashboard
-  let userLoggedIn = true;
+  let userLoggedIn = false;
 
   let loggedIn = (
     <>
-        <Redirect from="/" to="/profile" /> 
         <Nav />
           <Switch>
+            <Redirect exact from="/" to="/profile" /> 
             <Route path="/profile" component={Profile} />
             <Route path="/discover" component={Discover} />
             <Route path="/messages" component={Messages} />
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,16 +1,37 @@
-import React from "react"
-import { NavLink } from "react-router-dom"
+import React, { useState } from "react"
+import Button from '@material-ui/core/Button';
 import Following from "../Components/Following";
-
+import { makeStyles } from "@material-ui/core";
+// import photo1 from '../../assets/a0ebf9987c35f57f8bb9c8639b3a67fbd40ddaef.png'
 import "./Profile.css"
 
+const useStyles = makeStyles(theme => ({
+    button: {
+        // margin: theme.spacing(1),
+    },
+    activeButton: {
+        // margin: theme.spacing(1),
+        background: '#1976d2',
+        color: '#fff',
+        border: 'none'
+    }
+}));
+
 const Profile = () => {
+    const classes = useStyles();
+    const [toggleButton1, setToggleButton1] = useState(true);
+    const [toggleButton2, setToggleButton2] = useState(true);
+
+    // let toggleButton = () => {
+    //     setToggleClass = !toggleClass
+    // }
+
     return (
         <>
             <div className="profile">
                 <div className="profile-header">
                     <div className="headshot-container">
-                        <img src="assets/a0ebf9987c35f57f8bb9c8639b3a67fbd40ddaef.png" alt="Logo" />
+                        <img src='/assets/a0ebf9987c35f57f8bb9c8639b3a67fbd40ddaef.png' alt="Logo" />
                     </div>
                     <div className="name-container">
                         <h3>Jennifer</h3>
@@ -21,10 +42,8 @@ const Profile = () => {
                         </div>
                     </div>
                     <div className="follow-container">
-                        <ul>
-                            <NavLink to="/profile/follow" activeClassName="current"><li>Follow</li></NavLink>
-                            <NavLink to="/profile/message" activeClassName="current"><li>Message</li></NavLink>
-                        </ul>
+                        <Button onClick={(event) => setToggleButton1(!toggleButton1)} className={toggleButton1 ? classes.button : classes.activeButton}>Follow</Button>
+                        <Button onClick={(event) => setToggleButton2(!toggleButton2)} className={toggleButton2 ? classes.button : classes.activeButton}>Message</Button>
                     </div>
                 </div>
                 <Following />
