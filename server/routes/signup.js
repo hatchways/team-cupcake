9d98d8c28268c376600912a7d2ec4395c6d30e54
Benjamin Field <benjamin.field@gmail.com>
2019-11-13 10:52:21 -0500

    Removed methods being used in users.js

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -27,51 +27,6 @@ router.post("/", function(req, res) {
       res.status(401).send({ error: error.message });
     });
 });
-/**
- * I think this route should be also modified to include the password. !
- */
-// For testing, this likely shouldn't be here.
-// READ get user by username
-router.get("/:username", function(req, res) {
-  //
-  User.findOne({ username: req.params.username })
-    .then(function(user) {
-      if (user === null) {
-        //res.send('error: user not found')
-        throw "{error: user not found}";
-      }
-      res.send(user);
-    })
-    .catch(function(err) {
-      res.send({ err }); // likely too much information.  may want to select a key/value pair or two.
-    });
-});
-
-// UPDATE email or password
-router.put("/:username", function(req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-  // Might be better to use a switch, or some other logic, instead of the ifs
-  if (email) {
-    User.findOneAndUpdate({ username: req.params.username, email: email })
-      .then(function() {
-        res.send({ success: "email updated" });
-      })
-      .catch(err => {
-        res.send({ error: err });
-      });
-  } else if (password) {
-    User.findOneAndUpdate({ username: req.params.username, password: password })
-      .then(function() {
-        res.send({ success: "password updated" });
-      })
-      .catch(err => {
-        res.send({ error: err });
-      });
-  } else {
-    res.send({ error: "sorry bad update request" });
-  }
-});
 
 // DELETE user
 router.delete("/:username", function(req, res) {
