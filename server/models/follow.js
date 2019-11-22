d98b63a89bd0bd1582aff041299cad7116bb43e3
shojol shake <dip_shojol@yahoo.com>
2019-11-21 20:38:30 -0500

    clean up

--- server/models/follow.js
+++ server/models/follow.js
@@ -2,12 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const FollowSchema = new Schema({
-    // username is unique so let's use that for ID
-    /* If defaults added and not implements on User creation
-     * add setDefaultsOnInsert to users/PUT 
-     */
     userId: {
-        type: String, // either username or email, both unique
+        type: String,
         ref: 'User',
     },
     followers: [{
@@ -18,14 +14,7 @@ const FollowSchema = new Schema({
         type: String,
         ref: 'User',
     }]
-    // Possible added fields
-    // might want date created
-    // might want date last seen/active
 });
-
-// functions
-// maybe change description?
-// maybe change url
 
 const Follow = mongoose.model('follow', FollowSchema);
 
