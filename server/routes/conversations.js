0d21e8f903b43d9f5acd481d0860191ce0fa9787
Benjamin Field <benjamin.field@gmail.com>
2019-11-25 11:39:29 -0500

    Can't make populate work.  Argh.

--- server/routes/conversations.js
+++ server/routes/conversations.js
@@ -7,12 +7,11 @@ router.get("/:conversation_id/:page?", function(req, res) {
   // const { conversation_id, page } = match.params;
   const conversation_id = req.params.conversation_id;
   const page = req.params.page;
-  console.log(conversation_id);
-  const messagePerPage = 3;
-  // const skip = page ?
+  const messagePerPage = 3; // at 3 for testing, Aecio suggested 20
   const sliceValue = page ? messagePerPage * -1 * page : messagePerPage * -1;
   Conversation.findOne({ _id: conversation_id })
     .slice("messages", sliceValue, messagePerPage)
+    .populate("messages")
     .exec(function(err, messages) {
       if (err) {
         res.status(400).send({ error: err });
