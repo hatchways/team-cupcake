7fa6311dbe68883616e573d31e6692ea17927212
Benjamin Field <benjamin.field@gmail.com>
2019-11-29 13:58:56 -0500

    GET route for follow data needed for profile page

--- server/routes/profiles.js
+++ server/routes/profiles.js
@@ -1,6 +1,49 @@
 var express = require("express");
 var router = express.Router();
 const Profile = require("../models/profile");
+const User = require("../models/user");
+const Follow = require("../models/follow");
+
+// GET follow data for a profile. #Followers, #followees, etc.
+router.get("/follows/:profileUsername", function(req, res) {
+  const visitor_id = req.query.visitor;
+  let followData = {};
+  User.findOne({ username: req.params.profileUsername })
+    .select("_id")
+    .then(result => {
+      if (result === null) {
+        throw { error: "Bad User. Bad." };
+      } else {
+        followData.profileID = result._id; // or try followData['profileID']
+      }
+    })
+    .then(() => {
+      if (visitor_id !== null) {
+        Follow.findOne()
+          .and([{ follower: visitor_id }, { followee: followData.profileID }])
+          .then(result => {
+            if (result !== null) {
+              followData.visitorFollowsProfile = true;
+              Follow.countDocuments({ follower: followData.profileID }).then(
+                result => {
+                  followData.following = result;
+                  Follow.countDocuments({
+                    followee: followData.profileID
+                  }).then(result => {
+                    followData.followedBy = result;
+                    res.send({ result: followData });
+                  });
+                  // console.log(followData);
+                  //res.send({ result: followData });
+                }
+              );
+            }
+          });
+      } else {
+        throw "Bad visitor ID";
+      }
+    });
+});
 
 // GET user profile info
 router.get("/:username", function(req, res) {
