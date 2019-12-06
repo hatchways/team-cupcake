8c48deaa6dda4f6db4079f44c3ee29d63f756f6c
Badr Khalfaoui <badrkhalfaoui96@gmail.com>
2019-12-06 18:10:55 -0500

    tinechange

--- server/app.js
+++ server/app.js
@@ -56,16 +56,17 @@ app.use("/profile", profileRouter);
 app.use("/message", messageRouter);
 app.use("/conversation", auth, conversationRouter);
 app.use("/connectedusers", auth, connected);
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.redirect(path.join(__dirname, "client", "build", "index.html"));
   });
 }
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
 // error handler
 app.use(function(err, req, res, next) {
